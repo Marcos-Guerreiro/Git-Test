be3dcb6eebecc5a2f1daebdb62e1eb23c54c4388
Marcos-Guerreiro <marcos2003gh@gmail.com>
2024-10-20 12:30:58 -0300

    prueba de git

---
+++ POOgit.cpp
@@ -0,0 +1,7 @@
+#include <iostream> // Librería para manejar la entrada y salida de datos
+
+int main()
+{
+    std::cout << "Hello, World!" << std::endl; // Imprime "Hello, World!" y un salto de línea
+    return 0;                                  // Devuelve 0 para indicar que el programa terminó correctamente
+}
